ceab8b9d94e30c55b2fedc3f56343389a36b9844
Dimitar Hristoskov <mike_mail@mail.bg>
2015-07-03 13:18:26 +0300

    DistanceInLabyrinth

---
+++ LinearDataStructures/DistanceInLabyrinth/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceInLabyrinth
+{
+    internal struct Cell
+    {
+        public Cell(int x, int y, int value)
+            : this()
+        {
+            this.X = x;
+            this.Y = y;
+            this.Value = value;
+        }
+
+        public int Value { get; private set; }
+
+        public int Y { get; private set; }
+
+        public int X { get; private set; }
+    }
+}
