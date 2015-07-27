88ff423e1f39fb488c6991df2c09e2f480efc837
Dimitar Hristoskov <mike_mail@mail.bg>
2015-07-27 18:32:01 +0300

    Tree and Graph Traversal

--- TreeAndGraphTraversal/LongestPathInTree/LomgestPathTest.cs
+++ TreeAndGraphTraversal/LongestPathInTree/LomgestPathTest.cs
@@ -8,11 +8,13 @@ namespace LongestPathInTree
 {
     class LomgestPathTest
     {
+        static Dictionary<int, List<int>> tree = new Dictionary<int, List<int>>();
+        static Dictionary<int, int?> parents = new Dictionary<int, int?>();
+
         static void Main(string[] args)
         {
             int treeNodes = int.Parse(Console.ReadLine());
             int treeEdges = int.Parse(Console.ReadLine());
-            List<TreeNode> tree = new List<TreeNode>();
 
             for (int edge = 0; edge < treeEdges; edge++)
             {
@@ -20,35 +22,38 @@ namespace LongestPathInTree
                 int firstNode = edges[0];
                 int secondNode = edges[1];
 
-                var parent = new TreeNode(firstNode);
-                var child = new TreeNode(secondNode);
-
-                if (!tree.Contains(parent))
+                if (!tree.ContainsKey(firstNode))
                 {
-                    tree.Add(parent);
+                    tree[firstNode] = new List<int>();
                 }
-                //if (!tree.Contains(child))
-                //{
-                //    tree.Add(child);
-                //}
-                child.ParentNode = parent;                                
-                parent.AddChild(child);
-                //child.AddChild(parent);
-            }
-            Console.Clear();
-            PrintSet(tree);
+                if (!tree.ContainsKey(secondNode))
+                {
+                    tree[secondNode] = new List<int>();
+                }
+                if (!parents.ContainsKey(secondNode))
+                {
+                    parents[secondNode] = firstNode;
+                }
+                if (!parents.ContainsKey(firstNode))
+                {
+                    parents[firstNode] = null;
+                }
+
+                tree[firstNode].Add(secondNode);
+                parents[secondNode] = firstNode;
+            }        
         }
 
-        static void PrintSet(List<TreeNode> tree)
+        private static List<int> GetTreeLeaves()
         {
-            foreach (var node in tree)
-            {
-                Console.WriteLine(node.Value);
-                foreach (var child in node.Childrens)
-                {
-                    Console.Write(child.Value+ " ");
-                }
-            }
+            var leaves = tree.Where(node => node.Value.Count == 0).Select(node => node.Key).ToList();
+            return leaves;
+        }
+
+        private static int GetTreeRoot()
+        {
+            var root = parents.First(parent => parent.Value == null);
+            return root.Key;
         }
     }
 }
