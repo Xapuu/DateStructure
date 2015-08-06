d3c2799e6ace07c512b86d78f60ecef9f0be9e25
Dimitar Hristoskov <mike_mail@mail.bg>
2015-08-06 14:21:16 +0300

    Dictionaries and Hash Tables and Sets

---
+++ DictionariesHashTablesSets/OrderedSet/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderedSet
+{
+    class Node<T>
+    {
+        public Node(T value)
+        {
+            this.Value = value;
+            this.Parent = null;
+            this.LeftChild = null;
+            this.RightChild = null;
+        }
+
+        public T Value { get; set; }
+        public Node<T> Parent { get; set; }
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+    }
+}
